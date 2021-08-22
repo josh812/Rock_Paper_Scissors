cae114eb616df683be99e0ffe1e5f986ae708065
Josh <joshua.mccrory@icloud.com>
2021-08-21 17:52:25 -0700

    Added a UI with score

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Pick a random move
 function computerPlay() {
     let move;
     let number = Math.floor(Math.random() * 3);
@@ -13,35 +12,75 @@ function computerPlay() {
 }
 
 function oneRound(playerSelection, computerSelection) {
-    let output;
-
-    // Win or lose logic
+    container = document.querySelector('#results');
+    paras = container.querySelectorAll('p');
+    paras.forEach((para) => {
+        container.removeChild(para);
+    });
+    para = document.createElement('p');
     if (playerSelection === "rock" && computerSelection === "Scissors") {
-        output = "You Win! Rock beats Scissors!";
+        para.textContent = "You Win! Rock beats Scissors!";
+        para.setAttribute('id', 'results-text');
+        container.appendChild(para);
+        return 1;
     } 
     else if (playerSelection === "paper" && computerSelection === "Rock") {
-        output = "You Win! Paper beats Rock!";
+        para.textContent = "You Win! Paper beats Rock!";
+        para.setAttribute('id', 'results-text');
+        container.appendChild(para);
+        return 1;
     } 
     else if (playerSelection === "scissors" && computerSelection === "Paper") {
-        output = "You Win! Scissors beats Paper!";
+        para.textContent = "You Win! Scissors beats Paper!";
+        para.setAttribute('id', 'results-text');
+        container.appendChild(para);
+        return 1;
     } 
     else if (playerSelection === "rock" && computerSelection === "Paper") {
-        output = "You Lose! Paper beats Rock!";
+        para.textContent = "You Lose! Paper beats Rock!";
+        para.setAttribute('id', 'results-text');
+        container.appendChild(para);
+        return 0;
     } 
     else if (playerSelection === "paper" && computerSelection === "Scissors") {
-        output = "You Lose! Scissors beats Paper!";
+        para.textContent = "You Lose! Scissors beats Paper!";
+        para.setAttribute('id', 'results-text');
+        container.appendChild(para);
+        return 0;
     } 
     else if (playerSelection === "scissors" && computerSelection === "Rock") {
-        output = "You Lose! Rock beats Scissors!";
+        para.textContent = "You Lose! Rocks beats Scissors!";
+        para.setAttribute('id', 'results-text');
+        container.appendChild(para);
+        return 0;
     }
-    return output;
+    else {
+        para.textContent = "Tie!";
+        para.setAttribute('id', 'results-text');
+        container.appendChild(para);
+        return 2;
+    }
 }
 
 function game() {
-    while (true) {
-        let move = prompt("What's your move? Rock/Paper/Scissors")
-        console.log(oneRound(move.toLowerCase(), computerPlay()));
-    }
+    let player_score = 0;
+    let computer_score = 0;
+
+    const player_points = document.querySelector('#player-score');
+    const computer_points = document.querySelector('#computer-score');
+    btns = document.querySelectorAll('.btn');
+    btns.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            round = oneRound(btn.id, computerPlay());
+            if(round === 1) {
+                player_score++;
+            } else if(round === 0) {
+                computer_score++;
+            }
+            player_points.textContent = player_score;
+            computer_points.textContent = computer_score;
+        });
+    });
 }
 
 game()
